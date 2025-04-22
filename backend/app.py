d3aab1d024ee499361448de548a5ddc944c9be8c
via-lumina <haider.tux@gmail.com>
2025-04-22 20:00:16 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -39,4 +39,7 @@ def index():
     return "Via Lumina Backend API"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
